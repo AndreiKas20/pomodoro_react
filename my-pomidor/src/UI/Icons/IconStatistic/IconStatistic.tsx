2338d797a314618c8810a36a6df504a94f9743f9
Andrew <andrey.kashaev2018@yandex.ru>
2023-01-04 23:22:41 +0300

    svg

--- my-pomidor/src/UI/Icons/IconStatistic/IconStatistic.tsx
+++ my-pomidor/src/UI/Icons/IconStatistic/IconStatistic.tsx
@@ -8,7 +8,7 @@ interface IIcon {
 export function IconStatistic(props: IIcon) {
     return (
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0_16_639)">
+            <g clipPath="url(#clip0_16_639)">
                 <path d="M10 20H14V4H10V20ZM4 20H8V12H4V20ZM16 9V20H20V9H16Z" fill="#DC3E22"/>
             </g>
             <defs>
