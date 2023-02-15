d6029b59456012afc50027e7c05f5c52c1a235ec
andrey_ka <andrey.kashaev2018@yandex.ru>
2023-02-15 14:27:19 +0300

    non

--- my-pomidor/src/index.js
+++ my-pomidor/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
-const container = ReactDOM.createRoot(document.getElementById('root'));
-const root = ReactDOM.hydrateRoot(container, <App />);
-root.render(<App/>)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
